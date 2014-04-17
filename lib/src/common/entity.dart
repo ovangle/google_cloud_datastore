a70189f1a0d3e3915489ec2f6afc942d7cb9e10f
Thomas Stephenson <ovangle@gmail.com>
2014-04-17 17:35:35 +1000

    Moved check for property existence onto property map

--- lib/src/common/entity.dart
+++ lib/src/common/entity.dart
@@ -5,7 +5,7 @@ class Entity {
   final Key key;
   KindDefinition get kind => datastore.kindByName(key.kind);
   final PropertyMap _properties;
-  
+
   /**
    * Create a new [Entity] against the given [datastore]
    * with the given [key] and, optionally, initial values
@@ -15,36 +15,30 @@ class Entity {
     this.datastore = datastore,
     this.key = key,
     _properties = new PropertyMap(datastore.kindByName(key.kind), propertyInits);
-  
+
   dynamic getProperty(String propertyName) {
     var prop = _properties[propertyName];
-    if (prop == null) {
-      throw new NoSuchPropertyError(this, propertyName);
-    }
     return prop.value;
   }
-  
+
   void setProperty(String propertyName, var value) {
     var prop = _properties[propertyName];
-    if (prop == null) {
-      throw new NoSuchPropertyError(this, propertyName);
-    }
     prop.value = value;
   }
-  
+
   schema.Entity _toSchemaEntity() {
     schema.Entity schemaEntity = new schema.Entity();
     schemaEntity.key = key._toSchemaKey();
     _properties.forEach((String name, _PropertyInstance prop) {
       var defn = kind.properties[name];
       schemaEntity.property.add(prop._toSchemaProperty(defn));
-    });  
+    });
     return schemaEntity;
   }
-  
+
   bool operator ==(Object other) => other is Entity && other.key == key;
   int get hashCode => key.hashCode;
-  
+
   String toString() => "Entity($key)";
 }
 
@@ -54,7 +48,7 @@ class Entity {
 class EntityResult<T extends Entity> {
   static const KEY_ONLY = 0;
   static const ENTITY_PRESENT = 1;
-  
+
   /**
    * The looked up key
    */
@@ -64,18 +58,18 @@ class EntityResult<T extends Entity> {
    * or `null` if no entity corresponding with the given key exists.
    */
   final T entity;
-  
+
   bool get isKeyOnlyResult => resultType == KEY_ONLY;
-  
+
   bool get isPresent => resultType == ENTITY_PRESENT;
-  
+
   final resultType;
-  
+
   EntityResult._(this.resultType, this.key, this.entity);
-  
+
   factory EntityResult._fromSchemaEntityResult(
       Datastore datastore,
-      schema.EntityResult entityResult, 
+      schema.EntityResult entityResult,
       schema.EntityResult_ResultType resultType) {
     if (resultType == schema.EntityResult_ResultType.KEY_ONLY) {
       var key = new Key._fromSchemaKey(entityResult.entity.key);
@@ -95,7 +89,7 @@ class EntityResult<T extends Entity> {
 class PropertyMap extends UnmodifiableMapMixin<String,_PropertyInstance> {
   final KindDefinition kind;
   Map<String,_PropertyInstance> _entityProperties;
-  
+
   PropertyMap(KindDefinition kind, Map<String,dynamic> propertyInits) :
     this.kind = kind,
     _entityProperties = new Map.fromIterable(
@@ -103,9 +97,13 @@ class PropertyMap extends UnmodifiableMapMixin<String,_PropertyInstance> {
         key: (prop) => prop.name,
         value: (PropertyDefinition prop) => prop.type.create(initialValue: propertyInits[prop.name])
     );
-  
+
   @override
-  _PropertyInstance operator [](String key) => _entityProperties[key];
+  _PropertyInstance operator [](String key) {
+    if (!containsKey(key))
+      throw new NoSuchPropertyError(kind, key);
+    return _entityProperties[key];
+  }
 
   @override
   bool containsKey(String key) => _entityProperties.containsKey(key);
